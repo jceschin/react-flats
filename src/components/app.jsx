6d3e79301ff3edb680ead732d023a9656a4eb603
Juan Ceschin <juanceschin@gmail.com>
2020-09-26 12:07:24 -0300

    Pass flats object to FlatList

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import FlatList from './flat_list';
+import flats from './flats';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      flats: flats
+    };
+  }
+
   render () {
     return (
       <div className="container">
         <div className="left-scene">
-          <FlatList />
+          <FlatList flats={this.state.flats} />
         </div>
         <div className="rigth-scene"></div>
       </div>
