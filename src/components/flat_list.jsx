3c88ab7e5d6875a5b877200f9fbb7e078a24575d
Juan Ceschin <juanceschin@gmail.com>
2020-09-26 12:48:27 -0300

    Update falt markup

--- src/components/flat_list.jsx
+++ src/components/flat_list.jsx
@@ -4,7 +4,7 @@ import Flat from './flat';
 const FlatList = (props) => {
   return (
     <div className="flat-list">
-      {props.flats.map(flat => <Flat id={flat.id} key={flat.key} src={flat.imageUrl} />)}
+      {props.flats.map(flat => <Flat />)}
     </div>
   );
 };
