781fa38fcb46e4576553fd63ef91f9867804cec9
Juan Ceschin <juanceschin@gmail.com>
2020-09-26 12:11:03 -0300

    Add map method for props.flats

--- src/components/flat_list.jsx
+++ src/components/flat_list.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 
-const FlatList = () => {
-  return ();
-}
+const FlatList = (props) => {
+  return (
+    <div className="flat-list">
+      {props.flats.map(flat => <Flat id={flat.id} key={flat.key} />)}
+    </div>
+  );
+};
 
 export default FlatList;
