45393df4fc236b3fd1a67c90f317ad4aafa26d50
Juan Ceschin <juanceschin@gmail.com>
2020-09-26 11:51:42 -0300

    Create app layout. Map remaining

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import FlatList from './flat_list';
 
 class App extends Component {
   render () {
-
+    return (
+      <div className="container">
+        <div className="left-scene">
+          <FlatList />
+        </div>
+        <div className="rigth-scene"></div>
+      </div>
+    );
   }
 }
 
